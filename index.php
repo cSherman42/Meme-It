e0ec38899b82859812b4b0404a3c7015ee2dfddc
Christopher Sherman <csherman360@gmail.com>
2016-05-17 11:21:14 -0500

    Updating Index

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>( ͡° ͜ʖ ͡°)</title>
+  <title>OakWood</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="html/css" href="stylesheet.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/pure.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Temp Name</a>
+      <a class="navbar-brand" href="#">OakWood</a>
     </div>
     <div>
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.html">Home</a></li>
-        <li><a href="#">About</a></li>
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="opened.php">About</a></li>
         <li><a href="#">Logs</a></li>
         <li><a href="www.github.com/csherman42">My Github</a></li>
 		<li id="log"><form action="demo_form.asp">
@@ -40,14 +39,34 @@
   <ol>
 	<li><div class="linkBar">
 		<div class="actualLink">
-			<p id="one"> hello </p>
+			<a class="dj" href="opened.php?data=<?php echo $_POST['txtarea'];?>"id="one"><?php echo $_POST["titleInput"]; ?><br>
+	
+
+		
+ </a>
 		
 		</div> 
 	</div></li>
 	<li> <div class="linkBar">
 		<div class="actualLink">
-			<a class="links"href ="http://www.google.com"> hello </a>
+		  <p> </p>
+		</div>
+	</div> </li>
+	
+	<li> <div class="linkBar">
+		<div class="actualLink">
+		  <p> </p>
+		</div>
+	</div> </li>
+	
+	<li> <div class="linkBar">
+		<div class="actualLink">
+		  <p> </p>
+		</div>
 		
+	</div> </li><li> <div class="linkBar">
+		<div class="actualLink">
+		  <p> </p>
 		</div>
 	</div> </li>
 	</ol>
@@ -56,12 +75,12 @@
   <div class="row">
     <div class="col-md-3">
 		
-		<form action="submit.html">
+		<form action="submit.php">
 			<button type="submit" class="pure-button"> submit a link </button>
 		</form>
     </div>
     <div class="col-md-3"> 
-      <p>2</p>
+      <p>Help</p>
     </div>
     <div class="col-md-3"> 
      <p>  </p>
