e6d2d8f66165ae102b71781504f2263de20e2a30
Christopher Sherman <csherman360@gmail.com>
2016-05-17 11:23:08 -0500

    updating opened page

--- opened.php
+++ opened.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>( ͡° ͜ʖ ͡°)</title>
+  <title>OakWood</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="html/css" href="stylesheet.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/pure.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
@@ -15,15 +15,15 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Temp Name</a>
+      <a class="navbar-brand" href="#">OakWood</a>
     </div>
-    <div>
+    <div> 
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.html">Home</a></li>
+        <li class="active"><a href="index.php">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Logs</a></li>
         <li><a href="http://www.github.com/csherman42">My Github</a></li>
-		<li id="log"><form action="demo_form.asp">
+		<li id="log"><form action="gets.php">
 			<input type="text" name="fname" placeholder="username">
 			<input type="password" name="lname"placeholder="password"><br>
 			<input id="sub"type="submit" value="Submit">
@@ -39,15 +39,11 @@
   <div class="jumbotron">
   <ol>
 	<div class="linkBar">
-		<u><p style="text-align : center;"> Submit a Link </p></u>
+		<u><p style="text-align : center;">Opened Link</p></u>
 		<div class="actualLink">
-			<form class="pure-form" method="post">
-				<input id="titleInput"type="text" class="pure-input-1-2" placeholder="Title"><br>
-				<p class="spacerP"> </p>
-				<textarea id="txtarea"class="pure-input-1-2" placeholder="Text"></textarea> <br>
-				<p class="spacerP"> </p>
-				<button type="submit" class="pure-button pure-button-primary">Submit</button>
-			</form>
+			<?php echo $_GET['data']; ?>
+			
+			
 		</div> 
 	</div>
 	
